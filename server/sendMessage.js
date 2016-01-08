faf05ef23b36ac0ec3527a02478c313abdfa5e47
dououFullstack <13412133311@139.com>
2016-01-08 14:29:02 +0800

    chenge sendMessage.js

--- server/sendMessage.js
+++ server/sendMessage.js
@@ -1,22 +1,32 @@
 Meteor.methods({
+  getActivityInfo: function(activityId){
+    return Activities.findOne(activityId);
+  },
   multiSendMessage: function(list, content) {
     var token = Meteor.call("accessToken");
     var url = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=" + token;
+      console.log('==================================userId_list>', list);
 
     _.map(list, function(userId) {
+      console.log('==================================userId>', userId);
       var user = Meteor.users.findOne({_id: userId});
       var openid = "";
       if (user) {
         openid = user.openid;
+      console.log('==================================openID>', openid);
+
       }
       console.log("====> sending Message to ", openid);
       var a = {
         "touser": openid,
+        // "touser": "ogcWawd2oLZBkfxKtJ_5v0tVaH9Q",
         "msgtype": "text",
         "text": {
-          "content": user.profile.nickname + "同学，" + content
+          // "content": user.profile.nickname + "同学，" + content
+          "content": content
         }
       };
+      console.log('==================================', a);
       HTTP.post(url, {
         data: a
       }, function(err, res) {
@@ -31,7 +41,16 @@ Meteor.methods({
       });
     });
   },
-  multiSendNews: function(list, content) {
+  multiSendNews: function(content) {
+    // get all user list in database
+    var list = [];
+    var allUser = Meteor.users.find().fetch();
+    _.map(allUser, function(e){
+      if (e.profile.openid){
+        list.push(e._id);
+      }
+    });
+
     var token = Meteor.call("accessToken");
     var url = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=" + token;
 
@@ -49,16 +68,16 @@ Meteor.methods({
           "articles": [
             {
               "title": content.title,
-              "description": content.description,
+              "description": "",
               "url": content.url,
               "picurl": content.picurl
             },
-            // {
-            //   "title":"Happy Day",
-            //   "description":"Is Really A Happy Day",
-            //   "url":"URL",
-            //   "picurl":"PIC_URL"
-            // }
+            {
+              "title":"时间：" + content.time + "\n地点：" + content.where + "\n",
+              "description": "",
+              "url": content.url,
+              "picurl": ""
+            }
           ]
         }
       };
